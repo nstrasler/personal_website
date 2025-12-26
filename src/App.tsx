b22b5fab481587d90f6655e8c481d9923497f836
Noah <n.strasler@yahoo.com>
2025-12-26 17:56:54 -0500

    Added shadcn and navigate menubar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,32 @@ import Home from './pages/home.tsx'
 import About from './pages/about.tsx'
 import Projects from './pages/projects.tsx'
 
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
+
 const RootLayout = () => (
   <div className="layout">
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/projects">Projects</Link>
-    </nav>
+    <Menubar>
+      <MenubarMenu>
+        <MenubarTrigger>Navigate</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem asChild>
+            <Link to="/">Home</Link>
+          </MenubarItem>
+          <MenubarItem asChild>
+            <Link to="/about">About</Link>
+          </MenubarItem>
+          <MenubarItem asChild>
+            <Link to="/projects">Projects</Link>
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
     <main>
       <Outlet />
     </main>
