ebe175828b296ee62f23c4276eba4007a55221a5
Noah <n.strasler@yahoo.com>
2025-12-27 21:11:11 -0500

    Added database and form to submit to db

---
+++ src/pages/admin.tsx
@@ -0,0 +1,16 @@
+import '../index.css'
+
+function Admin() {
+  return (
+    <div className = "flex justify-center items-center h-screen">
+      <h1>
+        <p className="mb-4">
+          Noah Strasler</p>
+        <p>
+          Admin Page</p>
+      </h1>
+    </div>
+  )
+}
+
+export default Admin
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,15 +2,16 @@ import '../index.css'
 
 function Home() {
   return (
-    <div className = "flex justify-center items-center h-screen">
+  <>
+    <div className="flex justify-center items-center h-screen">
       <h1>
-        <p className="mb-4">
-          Noah Strasler</p>
-        <p>
-          Home Page</p>
+        <p className="mb-4 text-xl">Noah Strasler</p>
+        <p className="text-base">Home Page</p>
       </h1>
     </div>
-  )
+  </>
+)
+
 }
 
 export default Home
